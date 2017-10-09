3678874513ae17935a7f95f5132a0d59eb94486e
jinyoung <bmdj123@gmail.com>
2017-10-09 16:14:45 +0900

    1

--- src/commands.c
+++ src/commands.c
@@ -1,11 +1,14 @@
 #include <string.h>
-
+#include <unistd.h>
+#include <stdio.h>
 #include "commands.h"
-
+#define MAX_BUFF 1024
 int do_cd(int argc, char** argv) {
   if (!validate_cd_argv(argc, argv))
     return -1;
-
+  else
+	  chdir(argv[1]);
+	  return 0;
   // TODO: Fill it!
   return 0;
 }
@@ -13,18 +16,29 @@ int do_cd(int argc, char** argv) {
 int do_pwd(int argc, char** argv) {
   if (!validate_pwd_argv(argc, argv))
     return -1;
-
+  else{
+	  
+	  char buf[MAX_BUFF];
+	  getcwd(buf,MAX_BUFF);
+	  if(buf!=NULL){
+		  printf("%s\n",buf);
+		  return 0;
+	  }
+	 else return -1;	 
+	 }
   // TODO: Fill it!
 
-  return 0;
 }
 
 int validate_cd_argv(int argc, char** argv) {
-  // TODO: Fill it!
-  return 1;
+
+	if(strcmp(argv[0],"cd")!=0||argc!=2) return 0;
+	return 1;
 }
 
 int validate_pwd_argv(int argc, char** argv) {
-  // TODO: Fill it!
+	
+	if(strcmp(argv[0],"pwd")!=0||argc!=1) return 0;
+// TODO: Fill it!
   return 1;
 }
--- src/utils.c
+++ src/utils.c
@@ -1,7 +1,42 @@
 #include "utils.h"
-
+#include<string.h>
+#include<stdlib.h>
+#define MAX_SIZE 1000
 void mysh_parse_command(const char* command,
-                        int *argc, char*** argv)
+        int *argc, char*** argv)
 {
+
+	
+	char* temp;
+	
+	int count =0;
+	
+	temp=(char*)malloc(2048);
+	strcpy(temp,command);
+	char* token=strtok(temp," \n\t");
+
+	(*argv)=(char**)malloc(sizeof(char*)*MAX_SIZE);
+	for(int i=0;i<MAX_SIZE;i++){
+	
+		(*argv)[i]=(char*)malloc(sizeof(char)*MAX_SIZE);
+	}
+	
+	if(token==NULL){
+		*argc=1;
+		strcpy((*argv)[0],"");
+		return;
+	}
+	else{
+
+	while(token!=NULL){
+		strcpy((*argv)[count],token);
+		token=strtok(NULL," \n\t");
+		count++;
+	}
+	}
+	*argc=count;
+	free(temp);
+
+
   // TODO: Fill this!
 }
